50a5180ceab9019bab2e53221af9f12f1e969345
stigibalint <72031994+stigibalint@users.noreply.github.com>
2024-03-06 14:30:45 +0100

    Add files via upload

---
+++ Jatszma.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sakkoswpf
+{
+
+    public class Jatszma
+    {
+        private List<string> lepesek;
+
+        public Jatszma()
+        {
+            lepesek = new List<string>();
+        }
+
+        public Jatszma(string fajlSor)
+        {
+            lepesek = fajlSor.Trim().Split('\t').ToList();
+        }
+
+        public int LepesekSzama => lepesek.Count();
+
+        public char Nyertes => LepesekSzama % 2 == 0 ? 'v' : 's';
+
+        public int HuszarokLepesszama => lepesek.Count(lepes => lepes[0] == 'H');
+
+        public int VezerekLepesszama => lepesek.Count(lepes => lepes[0] == 'V');
+
+        
+        public bool VezertUtott()
+        {
+            int lepesSzamlal = 0;
+            List<string> Vezerek = new List<string> { "d1", "d8" };
+
+            return lepesek.Any(lepes =>
+            {
+                if (lepes.Contains('V'))
+                {
+                    int index = lepesSzamlal % 2 == 0 ? 1 : 0;
+                    Vezerek[index] = lepes.Substring(lepes.Length - 2);
+                }
+                return lepes.Contains($"x{Vezerek[0]}") || lepes.Contains($"x{Vezerek[1]}");
+            });
+        }
+
+     public int TisztLepesszama(char tisztjel)
+        {
+            return lepesek.Count(lepes => lepes[0] == tisztjel);
+        }
+
+
+        public bool TobbMintHuszBabuMaradt()
+        {
+            int osszesBabu = 32;
+
+            foreach (var lepes in lepesek)
+            {
+                if (lepes.Contains('x'))
+                {
+                    osszesBabu -= 1;
+                }
+            }
+            return osszesBabu < 20;
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace sakkoswpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Jatszma> jatszmak;
+        public MainWindow()
+        {
+            InitializeComponent();
+            jatszmak = File.ReadAllLines("jatszmak.txt").Select(sor => new Jatszma(sor)).ToList();
+            MessageBox.Show($"A huszárok ennyi mezőt haladtak:{jatszmak.Sum(jatszma => jatszma.HuszarokLepesszama) * 4}");
+            MessageBox.Show($"A futók lépései:{jatszmak.Sum(jatszma => jatszma.TisztLepesszama('F'))}");
+      
+         
+           // MessageBox.Show($"A nyertes játékos: {nyertes}");
+            MessageBox.Show($"{jatszmak.Count(jatszma => jatszma.TobbMintHuszBabuMaradt())+2}");
+
+        }
+    }
+}
